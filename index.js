cebbc8ef4308cb2cc59fcbadb5172f8e5188a5e4
swasti <swasti2901jain@gmail.com>
2022-06-10 15:03:55 +0530

    initial commit

--- index.js
+++ index.js
@@ -23,31 +23,11 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 app.use(expressLayouts);
 app.set('view engine','ejs');
-
-const http = require('http');
-const PORT = process.env.PORT || 3000;
-const HOST = process.env.HOST || 'localhost';
-
-var options = {
-  hostname: HOST,
-  port: PORT,
-  path: '?invalid=request', // This will trigger a client error and cause an H13
-  method: 'GET'
-};
-
-var req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.')
-  })
-});
-
-
+var https = require('https')
+var http = require('http')
+http.createServer(app).listen(80)
+https.createServer(options, app).listen(443)
+var PORT = process.env.PORT || 3000;
 
 app.get('/',(req, res)=>{
     res.render('services');
@@ -492,9 +472,4 @@ app.post('/htmltopdf',(req,res)=>{
   });
 });
 
-req.on('error', (e) => {
-  console.log(`problem with request: ${e.message}`);
-});
-
-req.end();
-
+app.listen(PORT, () => console.log("Port is listening..."))
